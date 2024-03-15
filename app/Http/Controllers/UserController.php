8cc8377aae34b760135b9a2d586a112f3a8ca449
Ignacio Andrade Salazar <ignacio.andrade8033@alumnos.udg.mx>
2024-03-15 12:56:59 -0600

    ya casi

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -78,7 +78,13 @@ class UserController extends Controller
     public function ingresar(Request $request)
     {
         $user = User::where('email', $request->email)->first();
-        return response()->json(['error' => 'Usuario encontrado']);
+//        return $user;
+        if($user != null){
+            return response()->json(['error' => 'Usuario encontrado']);
+        } else {
+
+        return response()->json(['error' => 'Usuario no encontrado']);
+        }
 
 //        if (!$user) {
 //            return response()->json(['error' => 'Usuario no encontrado']);
